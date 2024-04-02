995221c3b049bf7d48fdc195a45aea7ce7886898
Bereket Kibreab <bekibreab@gmail.com>
2024-04-02 14:02:18 +0400

    apply the functionality to the game

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [bestScore, setBestScore] = useState(0);
   const [cards, setCards] = useState([]);
   const [clikedIds, setClikedIds] = useState([]);
+  const [gameover, setGameover] = useState(false);
 
   useEffect(() => {
     fetchCards()
@@ -33,13 +34,28 @@ function App() {
   const handleClick = (id) => {
     if (!clikedIds.includes(id)) {
       setClikedIds((prevIds) => [...prevIds, id])
+      setScore(prevScore => prevScore + 1)
     } else {
-      setClikedIds([])
+      if (score > bestScore) {
+        setBestScore(score)
+      }
+      setGameover(true)
     }
-    console.log(clikedIds)
-    
+    setCards(prevCards => {
+      let shuffle = [...prevCards];
+      for (let i = prevCards.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [shuffle[i], shuffle[j]] = [shuffle[j], shuffle[i]];
+      }
+      return shuffle;
+    });
   }
   
+  const handleRestart = () => {
+    setGameover(false)
+    setScore(0)
+    setClikedIds([])
+  }
   
 
   return (
@@ -48,9 +64,18 @@ function App() {
         <h2>Memory Card Game</h2>
         <Scoreboard score={score} bestScore={bestScore}/>
       </header>
-      <main>
+      <main className={gameover ? 'blur' : ''}>
         <CardGrid cards={cards} onCardClick={handleClick}/>
       </main>
+      {gameover ?
+      <div className='gameover'>
+        <h3>Game Over</h3>
+        <h5>Score: {score}</h5>
+        <h5>Best Score: {bestScore}</h5>
+        <button onClick={handleRestart}>Restart</button>
+      </div> :
+      null}
+      
       
     </div>
   );
